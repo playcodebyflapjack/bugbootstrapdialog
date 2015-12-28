f6afe8a12451e22d0f9e5fc37fe68c8fe9589689
todsaporn yangdiew <todsapornyangdiew@gmail.com>
2015-12-28 10:25:29 +0700

    My test bug

---
+++ contant.php
@@ -0,0 +1,37 @@
+<div class="row" id="program_select" >
+	<div class="col-md-2" id="program_select">Program</div>
+	<div class="col-md-10">
+		<select id="program_selectep"  style="width: 100%">
+				<option value="">Mission Imposible 4 Break (1/8)</option>
+				<option value="">Mission Imposible 4 Break (2/8)</option>
+				<option value="">Mission Imposible 4 Break (3/8)</option>
+				<option value="">Mission Imposible 4 Break (4/8)</option>
+				<option value="">Mission Imposible 4 Break (5/8)</option>
+				<option value="">Mission Imposible 4 Break (6/8)</option>
+				<option value="">Mission Imposible 4 Break (7/8)</option>
+				<option value="">Mission Imposible 4 Break (8/8)</option>
+				<option value="">Game of Throne Session 1 EP 1 Break (1/8)</option>
+				<option value="">Game of Throne Session 1 EP 1 Break (2/8)</option>
+				<option value="">Game of Throne Session 1 EP 1 Break (3/8)</option>
+				<option value="">Game of Throne Session 1 EP 1 Break (4/8)</option>
+				<option value="">Game of Throne Session 1 EP 1 Break (5/8)</option>
+				<option value="">Game of Throne Session 1 EP 1 Break (6/8)</option>
+				<option value="">Game of Throne Session 1 EP 1 Break (7/8)</option>
+				<option value="">Game of Throne Session 1 EP 1 Break (8/8)</option>
+		</select>
+	</div>
+</div>
+
+<script type="text/javascript">
+
+	$(document ).ready(function() {
+
+
+	 	$("#program_selectep").select2({
+			theme: "bootstrap",
+		});
+
+
+	});
+
+</script>
